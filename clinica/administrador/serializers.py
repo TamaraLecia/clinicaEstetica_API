a3a969a4566064916d4ff65074576dc56de2e468
Lécia Tamara <leciatamara60@gmail.com>
2025-11-30 17:27:09 -0300

    django rest_framework instalado

---
+++ clinica/administrador/serializers.py
@@ -0,0 +1,54 @@
+from rest_framework import serializers
+
+from django.contrib.auth.models import User
+from .models import Administrador
+from profissional.models import Profissional
+from servico.models import Servico, TipoServico
+from cliente.models import Cliente
+from plano.models import Plano
+
+class AdministradorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Administrador
+        fields = "__all__"
+
+class AlterarSenhaSerializer(serializers.ModelSerializer):
+    class Meta:
+        username = serializers.CharField()
+        senha = serializers.CharField()
+
+        def save(self):
+            username = self.validated_data['username']
+            senha = self.validated_data['senha']
+            usuario = User.objects.get(username=username)
+            usuario.set_password(senha)
+            usuario.save()
+            return usuario
+
+
+
+
+class VerProfissionalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profissional
+        fields = "__all__"
+
+class MostrarServicoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Servico
+        fields = "__all__"
+
+class AlterarCategoriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TipoServico
+        fields = "__all__"
+
+class VerClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = "__all__"
+
+class VerPlanoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plano
+        fields = "__all__"
--- clinica/administrador/views.py
+++ clinica/administrador/views.py
@@ -1,85 +1,133 @@
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, redirect, render
-from django.contrib.auth.models import User
-from administrador.forms import AdministradorForm, EditAdmForm, SenhaForm
-from administrador.models import Administrador
-from plano.models import Plano
-from profissional.models import Profissional
-from cliente.models import Cliente
-from servico.models import Servico, TipoServico
-from servico.forms import EditCategoriaForm, EditServicoForm
-from servico.forms import EditServicoForm
-from clinicaEstetica.forms import AdicionarUsuarioForm, EditUsuarioForm
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required, permission_required
-from servico.utils import mostrarCategoria, mostrarMaisServicos, mostrarServico
-from django.urls import reverse
-from django.contrib.auth import get_user_model, logout
-from django.contrib.auth.models import Group
+#importações utilizadas de Api
+from rest_framework.views import APIView
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from .serializers import AdministradorSerializer, AlterarCategoriaSerializer, AlterarSenhaSerializer, MostrarServicoSerializer, VerClienteSerializer, VerPlanoSerializer, VerProfissionalSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status, permissions
 
+# importações do oauth2 - oauth-toolkit
+from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope
 # Create your views here.
 
-# #Index de administrador
-@login_required()
-@permission_required('administrador.view_administrador', raise_exception=True)
-def index (request):
-    return render(request, 'administrador/indexAdministrador.html',)
+#usando APIView do rest_framework
+class AdministradorAPIView(APIView):
+    # forma de realizar a authenticação com oauth-toolkit
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
 
-#Ver todos os administradores
-@login_required()
-@permission_required('administrador.detailhes_administrador', raise_exception=True)
-def verAdministrador (request):
-    administradores = Administrador.objects.select_related('user').all()
-    return render(request, 'administrador/verAdministrador.html', {'administradores' : administradores})
+    #ver todos os administradores
+    def get(self, request):
+        administradores = Administrador.objects.all()
+        serializer = AdministradorSerializer(administradores, many=True)
+        return Response(serializer.data)
+    
+    #adicionar administradores
+    def post(self, request):
+        serializer = AdministradorSerializer(data=request.data)
+        if serializer.is_valid():
+            administrador = serializer.save()
+            administrador.identificador = 'administrador'
+            administrador.save()
 
-#Ver todos os profissionais
-@login_required()
-@permission_required('administrador.detailhes_administrador', raise_exception=True)
-def verProfissional (request):
-    profissionais = Profissional.objects.select_related('user').all()
-    return render(request, 'administrador/verProfissional.html', {'profissionais' : profissionais})
+            grupo, _ = Group.objects.get_or_create(name='Administradores')
+            administrador.groups.add(grupo)
+            print("Administrador promovido a administrador:", administrador.nome)
 
-# ver e editar plano
-@login_required
-def verPlano(request):
-    planos = Plano.objects.all()  # ou com filtro
-    return render(request, 'administrador/verPlanos.html', {'verPlano': planos})
+            tornarAdmin(request, administrador.username)
+            print("Administrador salvo:", administrador.id)
+            return Response(serializer.data)
 
-#Ver todos os clientes
-@login_required()
-@permission_required('administrador.detailhes_administrador', raise_exception=True)
-def verCliente (request):
-    clientes = Cliente.objects.select_related('user').all()
-    return render(request, 'administrador/verClientes.html', {'clientes' : clientes})
+class AdministradorDetail(APIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+    
+    def get(self, request, id):
+        administrador = get_object_or_404(Administrador.objects.all(), id=id)
+        serializer = AdministradorSerializer(administrador)
+        return Response(serializer.data)
+    
+    #Update
+    def put(self, request, id):
+        administrador = get_object_or_404(Administrador.objects.all(), id=id)
+        serializer = AdministradorSerializer(administrador, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+    
+    #Delete
+    def delete(self, request, id):
+        administrador = get_object_or_404(Administrador.objects.all(), id=id)
+        administrador.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
+class AlterarSenha(APIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
 
-#Adicionar administrador
-@permission_required('administrador.add_administrador', raise_exception=True)
-def add_administrador(request):
-    form_user = AdicionarUsuarioForm(request.POST or None)
-    form = AdministradorForm(request.POST or None)
-    if form_user.is_valid() and form.is_valid():
-        user_administrador = form_user.save() #Cria o usuário do model user
-        administrador = form.save(commit=False) #Criar o usuario do model Administrador
-        administrador.user = user_administrador #Associar o usuario User com o Administrador
-        administrador.identificador = 'administrador'
-        administrador.save()
+    def put(self, request):
+        serializer = AlterarSenhaSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.errors, status=400)
 
-    #Adiciona o usuário ao grupo administrador -----------------------
-        nomeGrupo = administrador.identificador
-        grupo, _ = Group.objects.get_or_create(name='Administradores')
-        user_administrador.groups.add(grupo)
-        print("Administrador promovido a administrador:", administrador.nome)
-    #-----------------------------------------------------------------
-        print("Administrador criado:", administrador.id)
+class VerProfissional(APIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasScope]
 
-        tornarAdmin(request, user_administrador.username)
-        print("Administrador salvo:", administrador.id)
+    def get(self, request):
+        profissionais = Profissional
+        serializer = VerProfissionalSerializer(profissionais, many=True)
+        return Response(serializer.data)
 
-        return redirect('indexAdm')
-    return render(request, 'administrador/cadastrarAdminForm.html', {'form_user': form_user, 'form': form})
+class MostrarServico(APIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasScope]
 
-#tornar o usuário um administrador
+    def get(self, request):
+        servicos = Servico
+        serializer = MostrarServicoSerializer(servicos, many=True)
+        return Response(serializer.data)
+    
+class ServicoDetail(APIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+
+    def put(self, request, id):
+        servico = get_object_or_404(Servico.objects.all(), id=id)
+        serializer = MostrarServicoSerializer(servico, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+    def delete(self, request, id):
+        servico = get_object_or_404(Servico.objects.all(), id=id)
+        servico.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class AlterarCategoria(APIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+
+    def put(self, request, id):
+        categoria = get_object_or_404(TipoServico.objects.all(), id=id)
+        serializer = AlterarCategoriaSerializer(categoria, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+class VerCliente(APIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasScope]
+
+    def get(self, request):
+        clientes = Cliente
+        serializer = VerClienteSerializer(clientes, many=True)
+        return Response(serializer.data)
+
+class VerPlano(APIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasScope]
+
+    def get(self, request):
+        planos = Plano
+        serializer = VerPlanoSerializer(planos, many=True)
+        return Response(serializer.data)
+    
 def tornarAdmin(request, userName): 
     administrador = get_object_or_404(User, username = userName) 
     if administrador.is_superuser: 
@@ -92,160 +140,3 @@ def tornarAdmin(request, userName):
         messages.success(request, "Usuário promovido a administrador") 
     
     return redirect('indexAdm')
-
-#Update Adm
-@login_required
-@permission_required('administrador.change_administrador', raise_exception=True)
-def editarDadosAdmin(request, username):
-    administrador = get_object_or_404(Administrador, user__username=username)
-    user = administrador.user
-    
-    editarUserForm = EditUsuarioForm(request.POST, instance=user)
-    editarAdministradorForm = EditAdmForm(request.POST, instance=administrador)
-
-    if request.method == 'POST':
-        if editarUserForm.is_valid() and editarAdministradorForm.is_valid():
-            editarUserForm.save()
-            editarAdministradorForm.save()
-            return redirect('indexAdm')
-
-    return render(request, 'administrador/cadastrarAdminForm.html', {'editarUserForm' : editarUserForm, 'editarAdministradorForm' : editarAdministradorForm, 'Administrador' : administrador} )
-
-
-#Delete Adm
-@permission_required('administrador.delete_administrador', raise_exception=True)
-def deletarContaAdmin(request, username):
-    apagarAdministrador = get_object_or_404(Administrador, user__username=username)
-
-    administradorUser = apagarAdministrador.user
-    apagarAdministrador.delete()
-    administradorUser.delete()
-    return redirect('indexClinica')
-
-#Editar senha
-@permission_required('administrador.change_administrador', raise_exception=True)
-def editSenha(request, username):
-    User = get_user_model()
-    if request.user.is_authenticated:
-        verificarUsuario = Administrador.objects.filter(user__username=username).first()
-        if verificarUsuario and request.user.username == verificarUsuario.user.username:
-            if request.method == 'GET':
-                administradores = User.objects.all()
-                administrador= Cliente.objects.filter(user__username=username).first()
-                formSenha = SenhaForm(instance=administrador)
-
-                return render(request, 'administrador/senhaForm.html', {'formSenha' : formSenha ,'administradores': administradores})
-                
-            elif request.method == 'POST':
-                administradores = User.objects.all()
-                administrador = Administrador.objects.get(user__username=username)
-                formSenha = SenhaForm(request.POST, instance=administrador)
-
-                if formSenha.is_valid():
-                    formSenha.save()
-                        
-                    return redirect('indexAdm')
-                else:
-                    administradores = User.objects.all()
-        
-                    return render(request, 'administrador/senhaForm.html')
-        else:
-            messages.error(request, "Não é possivél alterar a senha de outro usuário")
-            return redirect('indexAdm')
-
-#Todos os serviços
-@permission_required('administrador.detailhes_administrador', raise_exception=True)
-def mostrarServicos(request):
-    servicos = {}
-    servicos.update(mostrarServico())
-    return render(request, 'administrador/todosServicos.html', servicos)
-
-#Deletar serviço
-@permission_required('administrador.delete_administrador', raise_exception=True)
-def deletarServico(request, id):
-    servico = Servico.objects.get(pk=id)
-
-    servico.delete()
-    return redirect('verServico')
-
-#Alterar serviço
-@permission_required('administrador.change_administrador', raise_exception=True)
-def alterarServico(request, id):
-    if request.method == 'GET':
-        servicos = Servico.objects.all()
-        servico = Servico.objects.filter(pk=id).first()
-        formServico = EditServicoForm(instance=servico)
-
-        return render(request, 'servico/servicoForm.html',{'formServico': formServico, 'servicos': servicos})
-    
-    elif request.method == 'POST':
-        servicos = Servico.objects.all()
-        servico = Servico.objects.get(pk=id)
-        formServico = EditServicoForm(request.POST, request.FILES, instance=servico)
-
-
-        if formServico.is_valid():
-            formServico.save()
-            return redirect('indexServico')
-        else:
-            servicos = Servico.objects.all()
-            return render(request, 'administrador/servicoForm.html')
-
-#Redireciona para o painel de serviços
-def redirecionarParaServico(request):
-    return redirect(reverse('indexServico'))
-
-
-#Redireciona para o painel de planos
-def redirecionaPlano(request):
-    return redirect(reverse('indexPlano'))
-
-
-#alterar categoria
-@permission_required('administrador.change_administrador', raise_exception=True)
-def alterarCategoria(request, id):
-    if request.method == 'GET':
-        categorias = TipoServico.objects.all()
-        categoria = TipoServico.objects.filter(pk=id).first()
-        formServico = EditCategoriaForm(instance=categoria)
-        return render(request, 'servico/servicoForm.html', {'formServico': formServico, 'categorias': categorias})
-    elif request.method == 'POST':
-        categorias = TipoServico.objects.all()
-        categoria = TipoServico.objects.get(pk=id)
-        formServico = EditCategoriaForm(request.POST, request.FILES, instance=categoria)
-
-        if formServico.is_valid():
-            formServico.save()
-            return redirect('indexServico')
-        else:
-            categorias = TipoServico.objects.all()
-            return render(request, 'servico/servicoForm.html')
-        
-
-# Deletar Categoria
-@permission_required('administrador.delete_administrador', raise_exception=True)
-def deletarCategoria(request, id):
-    categoria = TipoServico.objects.get(pk=id)
-    categoria.delete()
-
-    return ("indexAdm")
-
-
-#DeletarProfissional
-@login_required()
-def deletarProfissional(request, username):
-    #pega o profissional pelo o username
-    apagarProfissional= get_object_or_404(Profissional, user__username =username)
-  
-    profissionalUser = apagarProfissional.user
-    apagarProfissional.delete()
-    profissionalUser.delete()
-    return redirect(reverse('indexAdm'))
-
-#realizar logout
-@login_required
-def realizarLogout(request):
-    logout(request)
-    return redirect('indexClinica')
-
-
