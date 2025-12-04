5569b2812cc65ee5c2b5a096d0c51442e53949d6
Tamara Lécia <tamaralecia976@gmail.com>
2025-12-04 11:28:25 -0300

    API de Administrador e Serviço funcionando, após tentativa de apagar os commits do trabalho de web1

--- clinica/servico/urls.py
+++ clinica/servico/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path 
 from .import views
+from .views import ServicoAPIView 
 
 urlpatterns = [
-    path("index/", views.index, name="indexServico"),
+    path("index/", ServicoAPIView.as_view(), name="indexServico"),
     path("addServico/", views.addServico, name="addServico"),
     path("alterarServico/<int:id>/", views.alterarServico, name="alterarServico"),
     path("deletarServico/<int:id>/", views.deletarServico, name="deletarServico"),
-    path("addCategoria/",views.addCategoria, name="addCategoria"),
+    # path("addCategoria/",views.addCategoria, name="addCategoria"),
     path("alterarCategoria/<int:id>/", views.alterarCategoria, name="alterarCategoria"),
     path("deletarCategoria/<int:id>/", views.deletarCategoria, name="deletarCategoria"),
     #path("mostrarCategoria/", views.mostrarCategoria, name="mostrarCategoria"),
