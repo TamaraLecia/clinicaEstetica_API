ccc7b146cd05e032a92e4589e84dad30544d932a
Lécia Tamara <leciatamara60@gmail.com>
2025-12-05 11:56:56 -0300

    correção da importação do serializer de plano

--- clinica/plano/urls.py
+++ clinica/plano/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path 
 from .import views
-# from .views import PlanoAPIView, PlanoUpdateAPIView
+from .views import PlanoAPIView, PlanoUpdateAPIView
 
 urlpatterns = [
-    # path('api/planos/', PlanoAPIView.as_view(), name='api_planos'),
-    # path('api/plano/<int:id>/deletar/', PlanoUpdateAPIView.as_view(), name='api_deletar_plano'),
-#     path("index/", views.index, name="indexPlano"),
-#     path("addPlano/", views.addPlano, name="addPlano"),
-#     path("alterarPlano/<int:id>/", views.alterarPlano, name="alterarPlano"),
-#     path("deletarPlano/<int:id>/", views.deletarPlano, name="deletarPlano"),
-#     path("redirecionaParaAdministrador/", views.redirecionarParaAdministrador, name="redirecionarParaAdministrador"),
-#     path("mostrarPlano/", views.mostrarPlano, name="mostrarPlano")
+    path('api/planos/', PlanoAPIView.as_view(), name='api_planos'),
+     path('api/plano/<int:id>/deletar/', PlanoUpdateAPIView.as_view(), name='api_deletar_plano'),
+    path("index/", views.index, name="indexPlano"),
+    path("addPlano/", views.addPlano, name="addPlano"),
+     path("alterarPlano/<int:id>/", views.alterarPlano, name="alterarPlano"),
+     path("deletarPlano/<int:id>/", views.deletarPlano, name="deletarPlano"),
+     path("redirecionaParaAdministrador/", views.redirecionarParaAdministrador, name="redirecionarParaAdministrador"),
+    path("mostrarPlano/", views.mostrarPlano, name="mostrarPlano")
 ]
--- clinica/plano/views.py
+++ clinica/plano/views.py
@@ -4,177 +4,111 @@ from plano.models import Plano
 from plano.forms import EditPlanoForm, PlanoForm
 from django.contrib.auth.decorators import permission_required
 
-#IMPORTAÇÕES DE API
+# IMPORTAÇÕES DE API
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.parsers import MultiPartParser, FormParser
-# from .serializers import PlanoSerializer
+from .serializer import PlanoSerializer
 from rest_framework import generics
 
-# class PlanoAPIView(APIView):
-#     parser_classes = [MultiPartParser, FormParser]
-# class PlanoAPIView(APIView):
-#     parser_classes = [MultiPartParser, FormParser]
 
-#     # LISTAR TODOS OS PLANOS
-#     def get(self, request):
-#         planos = Plano.objects.all()
-#         serializer = PlanoSerializer(planos, many=True)
-#         return Response(serializer.data)
-#     # LISTAR TODOS OS PLANOS
-#     def get(self, request):
-#         planos = Plano.objects.all()
-#         serializer = PlanoSerializer(planos, many=True)
-#         return Response(serializer.data)
+class PlanoAPIView(APIView):
+    parser_classes = [MultiPartParser, FormParser]
 
-#     # CRIAR UM NOVO PLANO
-#     def post(self, request):
-#         serializer = PlanoSerializer(data=request.data)
-#     # CRIAR UM NOVO PLANO
-#     def post(self, request):
-#         serializer = PlanoSerializer(data=request.data)
+    # LISTAR TODOS OS PLANOS
+    def get(self, request):
+        planos = Plano.objects.all()
+        serializer = PlanoSerializer(planos, many=True)
+        return Response(serializer.data)
 
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(
-#                 {
-#                     "mensagem": "Plano criado com sucesso!",
-#                     "plano": serializer.data
-#                 },
-#                 status=status.HTTP_201_CREATED
-#             )
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(
-#                 {
-#                     "mensagem": "Plano criado com sucesso!",
-#                     "plano": serializer.data
-#                 },
-#                 status=status.HTTP_201_CREATED
-#             )
+    # CRIAR UM NOVO PLANO
+    def post(self, request):
+        serializer = PlanoSerializer(data=request.data)
 
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-# # EDITAR um plano específico (GET, PUT, PATCH)
-# class PlanoUpdateAPIView(generics.RetrieveUpdateAPIView):
-#     queryset = Plano.objects.all()
-#     serializer_class = PlanoSerializer
-#     lookup_field = 'id'
-# # EDITAR um plano específico (GET, PUT, PATCH)
-# class PlanoUpdateAPIView(generics.RetrieveUpdateAPIView):
-#     queryset = Plano.objects.all()
-#     serializer_class = PlanoSerializer
-#     lookup_field = 'id'
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {
+                    "mensagem": "Plano criado com sucesso!",
+                    "plano": serializer.data
+                },
+                status=status.HTTP_201_CREATED
+            )
 
-# # DELETAR um plano específico
-# class PlanoDeleteAPIView(generics.DestroyAPIView):
-#     queryset = Plano.objects.all()
-#     serializer_class = PlanoSerializer
-#     lookup_field = 'id'
-# # DELETAR um plano específico
-# class PlanoDeleteAPIView(generics.DestroyAPIView):
-#     queryset = Plano.objects.all()
-#     serializer_class = PlanoSerializer
-#     lookup_field = 'id'
-
-# #Criar plano
-# @permission_required('plano.add_plano', raise_exception=True)
-# def addPlano(request):
-#     formPlano = PlanoForm(request.POST)
-# #Criar plano
-# @permission_required('plano.add_plano', raise_exception=True)
-# def addPlano(request):
-#     formPlano = PlanoForm(request.POST)
-
-#     if formPlano.is_valid():
-#         plano = formPlano.save(commit=False)
-#         plano.save()
-#         formPlano.save_m2m()
-#         return redirect('indexPlano')
-#     return render(request, 'plano/planoForm.html',{'formPlano': formPlano})
-#     if formPlano.is_valid():
-#         plano = formPlano.save(commit=False)
-#         plano.save()
-#         formPlano.save_m2m()
-#         return redirect('indexPlano')
-#     return render(request, 'plano/planoForm.html',{'formPlano': formPlano})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# #Ver planos
-# def index(request):
-#     return render(request, 'plano/indexPlano.html')
-# #Ver planos
-# def index(request):
-#     return render(request, 'plano/indexPlano.html')
+# EDITAR um plano específico (GET, PUT, PATCH)
+class PlanoUpdateAPIView(generics.RetrieveUpdateAPIView):
+    queryset = Plano.objects.all()
+    serializer_class = PlanoSerializer
+    lookup_field = 'id'
 
-# #Editar planos
-# @permission_required('plano.change_plano', raise_exception=True)
-# def alterarPlano(request, id):
-#     if request.method == 'GET':
-#         planos = Plano.objects.all()
-#         plano = Plano.objects.filter(pk=id).first()
-#         formPlano = EditPlanoForm(instance=plano)
-# #Editar planos
-# @permission_required('plano.change_plano', raise_exception=True)
-# def alterarPlano(request, id):
-#     if request.method == 'GET':
-#         planos = Plano.objects.all()
-#         plano = Plano.objects.filter(pk=id).first()
-#         formPlano = EditPlanoForm(instance=plano)
 
-#         return render(request, 'plano/planoForm.html',{'formPlano': formPlano, 'planos': planos})
-#         return render(request, 'plano/planoForm.html',{'formPlano': formPlano, 'planos': planos})
-    
-#     elif request.method == 'POST':
-#         planos = Plano.objects.all()
-#         plano = Plano.objects.get(pk=id)
-#         formPlano = EditPlanoForm(request.POST,instance=plano)
-#     elif request.method == 'POST':
-#         planos = Plano.objects.all()
-#         plano = Plano.objects.get(pk=id)
-#         formPlano = EditPlanoForm(request.POST,instance=plano)
+# DELETAR um plano específico
+class PlanoDeleteAPIView(generics.DestroyAPIView):
+    queryset = Plano.objects.all()
+    serializer_class = PlanoSerializer
+    lookup_field = 'id'
 
-#         if formPlano.is_valid():
-#             formPlano.save()
-#             return redirect('indexAdm')
-#         else:
-#             planos = Plano.objects.all()
-#             return render(request, 'plano/planoForm.html')
-#         if formPlano.is_valid():
-#             formPlano.save()
-#             return redirect('indexAdm')
-#         else:
-#             planos = Plano.objects.all()
-#             return render(request, 'plano/planoForm.html')
 
-# #Deletar palanos
-# @permission_required('plano.delete_plano', raise_exception=True)
-# def deletarPlano(request, id):
-#     form = Plano.objects.get(pk=id)
-# #Deletar palanos
-# @permission_required('plano.delete_plano', raise_exception=True)
-# def deletarPlano(request, id):
-#     form = Plano.objects.get(pk=id)
+# Criar plano
+@permission_required('plano.add_plano', raise_exception=True)
+def addPlano(request):
+    formPlano = PlanoForm(request.POST)
 
-#     form.delete()
-#     return redirect('indexAdm')
-#     form.delete()
-#     return redirect('indexAdm')
+    if formPlano.is_valid():
+        plano = formPlano.save(commit=False)
+        plano.save()
+        formPlano.save_m2m()
+        return redirect('indexPlano')
 
-# #Redireciona para o painel de administrador
-# def redirecionarParaAdministrador(request):
-#     return redirect(reverse('indexAdm'))
-# #Redireciona para o painel de administrador
-# def redirecionarParaAdministrador(request):
-#     return redirect(reverse('indexAdm'))
+    return render(request, 'plano/planoForm.html', {'formPlano': formPlano})
 
-# #Mostrar planos para cliente
-# def mostrarPlano(request):
-#     planos = Plano.objects.all()  # ou com filtro
-#     return render(request, 'plano/__pricingStart.html', {'verPlano': planos})
-# #Mostrar planos para cliente
-# def mostrarPlano(request):
-#     planos = Plano.objects.all()  # ou com filtro
-#     return render(request, 'plano/__pricingStart.html', {'verPlano': planos})
+
+# Ver planos
+def index(request):
+    return render(request, 'plano/indexPlano.html')
+
+
+# Editar planos
+@permission_required('plano.change_plano', raise_exception=True)
+def alterarPlano(request, id):
+    if request.method == 'GET':
+        planos = Plano.objects.all()
+        plano = Plano.objects.filter(pk=id).first()
+        formPlano = EditPlanoForm(instance=plano)
+
+        return render(request, 'plano/planoForm.html', {'formPlano': formPlano, 'planos': planos})
+
+    elif request.method == 'POST':
+        planos = Plano.objects.all()
+        plano = Plano.objects.get(pk=id)
+        formPlano = EditPlanoForm(request.POST, instance=plano)
+
+        if formPlano.is_valid():
+            formPlano.save()
+            return redirect('indexAdm')
+        else:
+            planos = Plano.objects.all()
+            return render(request, 'plano/planoForm.html')
+
+
+# Deletar planos
+@permission_required('plano.delete_plano', raise_exception=True)
+def deletarPlano(request, id):
+    form = Plano.objects.get(pk=id)
+    form.delete()
+    return redirect('indexAdm')
+
+
+# Redireciona para o painel de administrador
+def redirecionarParaAdministrador(request):
+    return redirect(reverse('indexAdm'))
+
+
+# Mostrar planos para cliente
+def mostrarPlano(request):
+    planos = Plano.objects.all()
+    return render(request, 'plano/__pricingStart.html', {'verPlano': planos})
