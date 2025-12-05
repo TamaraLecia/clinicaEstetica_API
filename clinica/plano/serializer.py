742d17c24199fc65be04a5a968460493934a1e23
Leomira-Couto <leomyraoliveira@gmail.com>
2025-12-05 19:59:29 -0300

    Implementado create e update no serializer de Plano

--- clinica/plano/serializer.py
+++ clinica/plano/serializer.py
@@ -3,6 +3,20 @@ from plano.models import Plano
 from django.contrib.auth.models import User
 
 class PlanoSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Plano
-        fields = '__all__'
+        fields = '_all_'
+
+    # Função para criar um plano
+    def create(self, validated_data):
+        plano = Plano.objects.create(**validated_data)
+        return plano
+
+    # Função para editar/atualizar um plano
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
