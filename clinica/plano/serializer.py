7515e6a001b220e8e06a3582ebed7cdb4c13fdc3
Lécia Tamara <leciatamara60@gmail.com>
2025-12-05 18:53:19 -0300

    ajustes de html

--- clinica/plano/serializer.py
+++ clinica/plano/serializer.py
@@ -1,12 +1,8 @@
+from rest_framework import serializers
+from plano.models import Plano
+from django.contrib.auth.models import User
 
-
-
-
-# from rest_framework import serializers
-# from plano.models import Plano
-# from django.contrib.auth.models import User
-
-# class PlanoSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Plano
-#         fields = '__all__'
+class PlanoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plano
+        fields = '__all__'
